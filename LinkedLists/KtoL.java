86cbcc146b9fdce9c06544d54230a085c4c4d9ec
AkshayRaman97 <akshay.raman97@gmail.com>
2018-03-31 12:31:59 +0530

    Completed K to Last 2.2

--- LinkedLists/KtoL.java
+++ LinkedLists/KtoL.java
@@ -4,13 +4,12 @@ import java.util.*;
 public class KtoL {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		in.delimitter();
 		System.out.println("Enter data to be inserted");
-		System.out.println("Enter no data to exit");
+		System.out.println("Enter x to exit");
 		Node head = new Node();
 		Node n = head;
 		int count=0;
-		while(in.hasNextInt()){
+		while(true){
 			try{
 			int data = in.nextInt();
 			n.data = data;
@@ -19,21 +18,27 @@ public class KtoL {
 			count++;
 			}
 			catch(InputMismatchException ime){
+				in.nextLine();
 				System.out.println("Exiting loop...");
 				break;
 			}
 		}
-		System.out.print("Enter Kth to last element to be retrieved : ");
-		int k = count - in.nextInt()-1;
 		n = head;
-		if(k<=0){
+		System.out.println("Your List :");
+		while(n.next!=null){
+			System.out.print(n.data + " ");
+			n = n.next;
+		}
+		System.out.print("\nEnter Kth to last element to be retrieved : ");
+		int k = count-in.nextInt();
+		n = head;
+		if(k<1){
 			System.out.println("Invalid k");
 		}else{
 			while(k-->0){
 				n = n.next;
 			}
+			System.out.println("Element retrieved  : " + n.data);
 		}
-		System.out.println("Element retrieved  : " + n.data);
-
 	}
 }
